3fd6062e323aee41225883ae38f8b9b123c09ea8
Quartezz <kamil.machel03@gmail.com>
2023-09-04 21:09:33 +0200

    Created function for adding the nav container with buttons to the page

--- src/mainpage.js
+++ src/mainpage.js
@@ -7,9 +7,47 @@ function createHeader() {
     title.textContent = "Stateside Eats & Vibes";
 
     header.appendChild(title);
+    header.appendChild(createNav());
 
 
     return header;
+}
+
+function createNav() {
+    const nav = document.createElement("nav");
+    
+    const homeButton = document.createElement("button");
+    homeButton.classList.add("button-nav");
+    homeButton.textContent = "Home";
+    homeButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(homeButton);
+        loadHome();
+    });
+
+    const menuButton = document.createElement("button");
+    menuButton.classList.add("button-nav");
+    menuButton.textContent = "Menu";
+    menuButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(menuButton);
+        loadMenu();
+    });
+
+    const contactButton = document.createElement("button");
+    contactButton.classList.add("button-nav");
+    contactButton.textContent = "Contact";
+    contactButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(contactButton);
+        loadContact();
+    });
+
+    nav.appendChild(homeButton);
+    nav.appendChild(menuButton);
+    nav.appendChild(contactButton);
+
+    return nav;
 }
 
 function createWebsite() {
