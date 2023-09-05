43bcbcc57e91aa969f3f12bfb4f6a8647b10cc03
Quartezz <kamil.machel03@gmail.com>
2023-09-06 01:32:23 +0200

    added 'button' to function setActiveButton

--- src/mainpage.js
+++ src/mainpage.js
@@ -11,60 +11,60 @@ function createHeader() {
     title.textContent = "Stateside Eats & Vibes";
 
     header.appendChild(title);
-    //header.appendChild(createNav());
+    header.appendChild(createNav());
 
 
     return header;
 }
 
-// function createNav() {
-//     const nav = document.createElement("nav");
+ function createNav() {
+     const nav = document.createElement("nav");
     
-//     const homeButton = document.createElement("button");
-//     homeButton.classList.add("button-nav");
-//     homeButton.textContent = "Home";
-//     homeButton.addEventListener("click", (e) => {
-//         if (e.target.classList.contains("active")) return;
-//         setActiveButton(homeButton);
-//         loadHome();
-//     });
+    const homeButton = document.createElement("button");
+    homeButton.classList.add("button-nav");
+    homeButton.textContent = "Home";
+    homeButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(homeButton);
+        loadHome();
+    });
 
-//     const menuButton = document.createElement("button");
-//     menuButton.classList.add("button-nav");
-//     menuButton.textContent = "Menu";
-//     menuButton.addEventListener("click", (e) => {
-//         if (e.target.classList.contains("active")) return;
-//         setActiveButton(menuButton);
-//         loadMenu();
-//     });
+    const menuButton = document.createElement("button");
+    menuButton.classList.add("button-nav");
+    menuButton.textContent = "Menu";
+    menuButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(menuButton);
+        loadMenu();
+    });
 
-//     const contactButton = document.createElement("button");
-//     contactButton.classList.add("button-nav");
-//     contactButton.textContent = "Contact";
-//     contactButton.addEventListener("click", (e) => {
-//         if (e.target.classList.contains("active")) return;
-//         setActiveButton(contactButton);
-//         loadContact();
-//     });
+    const contactButton = document.createElement("button");
+    contactButton.classList.add("button-nav");
+    contactButton.textContent = "Contact";
+    contactButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setActiveButton(contactButton);
+        loadContact();
+    });
 
-//     nav.appendChild(homeButton);
-//     nav.appendChild(menuButton);
-//     nav.appendChild(contactButton);
+    nav.appendChild(homeButton);
+    nav.appendChild(menuButton);
+    nav.appendChild(contactButton);
 
-//     return nav;
-// }
+    return nav;
+}
 
-// function setActiveButton() {
-//     const buttons = document.querySelectorAll(".button-nav");
+function setActiveButton(button) {
+    const buttons = document.querySelectorAll(".button-nav");
 
-//     buttons.forEach((button) => {
-//         if (button !== this) {
-//             button.classList.remove("active");
-//         }
-//     });
+    buttons.forEach((button) => {
+        if (button !== this) {
+            button.classList.remove("active");
+        }
+    });
 
-//     button.classList.add("active");
-// }
+    button.classList.add("active");
+}
 
 function createMain() {
     const main = document.createElement("main");
@@ -103,7 +103,7 @@ function createWebsite() {
     content.appendChild(createMain());
     content.appendChild(createFooter());
 
-    //setActiveButton(document.querySelector(".button-nav"));
+    setActiveButton(document.querySelector(".button-nav"));
     loadHome();
 }
 
